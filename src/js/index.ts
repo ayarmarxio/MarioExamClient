05f644e8c3164fa835dcd586c5531f39d914357c
Mario Andres Ravina <ayarmarxio@gmail.com>
2019-01-03 21:14:11 +0100

    marioexam ready

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,47 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+import axios, {
+    AxiosResponse,
+    AxiosError,
+} from "../../node_modules/axios/index";
+
+import { json2table100 } from "./generictable";
+
+let btn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("first-btn");
+btn.addEventListener("click", firstFunction );
+
+
+function firstFunction () {
+
+}
+// more advanced object, including nested objects (and double nested objects)
+interface IUser {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: IAddress;
+    phone: string;
+    company: { name: string; catchPhrase: string; bs: string };
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
+interface IAddress {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: { lat: string; long: string };
 }
-let user: Person = { firstName: "John", lastName: "Doe" };
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+axios.get<IUser[]>("http://jsonplaceholder.typicode.com/users")
+    .then(function (response: AxiosResponse<IUser[]>): void {
+        let data: IUser[] = response.data;
+        console.log(data);
+        let result: string = json2table100(response.data);
+        console.log(result);
+        let element: HTMLDivElement = <HTMLDivElement>document.getElementById("content");
+        element.innerHTML = result;
+    })
+    .catch(function (error: AxiosError): void {
+        console.log(JSON.stringify(error));
+    });
+
+
